bff7740059103a5a911bf4358ae43ecefa869131
AntaresMugisho <antaresmugisho@gmail.com>
2022-12-03 00:35:38 +0200

    Starting slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 //-------------------------------------------------------
 // Humburger menu toggle on mobile
 const humburger = document.querySelector(".humburger")
-humburger.addEventListener('click', toggle_menu)
+humburger.addEventListener("click", toggle_menu)
 
 function toggle_menu(){
     console.log('Clicked')
@@ -18,3 +18,17 @@ const nav_links = document.getElementsByClassName("nav-link")
 for (const nav_link of nav_links){
     nav_link.addEventListener("click", toggle_menu)
 }
+
+//Image slider
+const img_container = document.querySelector(".image-container")
+
+const prev_btn = document.querySelector(".zap-btn.prev")
+prev_btn.addEventListener("click", ()=>{
+    img_container.scrollLeft=img_container.scrollLeft+200
+})
+
+const next_btn = document.querySelector(".zap-btn.next")
+next_btn.addEventListener("click", ()=>{
+    img_container.scrollLeft=img_container.scrollLeft-200
+})
+
