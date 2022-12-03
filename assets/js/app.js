e831d75a7de13ae4bb7e9b8feb123ae059f203ef
AntaresMugisho <antaresmugisho@gmail.com>
2022-12-03 10:54:30 +0200

    Now image slider works

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,82 @@
-
-//-------------------------------------------------------
+//+--------------------------------+
+//| NAV MENU ON MOBILE             |
+//+--------------------------------+
 // Humburger menu toggle on mobile
 const humburger = document.querySelector(".humburger")
 humburger.addEventListener("click", toggle_menu)
 
 function toggle_menu(){
-    console.log('Clicked')
     const navmenu = document.querySelector(".nav-menu")
     navmenu.classList.toggle("nav-menu--show")
-
     humburger.classList.toggle("bi-list")
     humburger.classList.toggle("bi-x")
 }
 
-// Hide navmenu when clicking on a link in navmenu
+// Hide navmenu when clicking on a link of navmenu
 const nav_links = document.getElementsByClassName("nav-link")
 for (const nav_link of nav_links){
     nav_link.addEventListener("click", toggle_menu)
 }
+ 
+//+--------------------------------+
+//| IMAGE CONTAINER (SLIDER)       |
+//+--------------------------------+
 
-//Image slider
 const img_container = document.querySelector(".image-container")
+
+// Scroll with arrow buttons
+const next_btn = document.querySelector(".zap-btn.next")
+next_btn.addEventListener("click", ()=>{
+    img_container.scrollLeft += 200
+})
 
 const prev_btn = document.querySelector(".zap-btn.prev")
 prev_btn.addEventListener("click", ()=>{
-    img_container.scrollLeft=img_container.scrollLeft+200
+    
+    if (img_container.scrollLeft == 1191){
+        img_container.scrollLeft -= 191
+        // 1191 is the max scrollLeft value
+    }
+    else{
+        img_container.scrollLeft -= 200
+    }
 })
 
-const next_btn = document.querySelector(".zap-btn.next")
-next_btn.addEventListener("click", ()=>{
-    img_container.scrollLeft=img_container.scrollLeft-200
+// Update active image indicator on scroll
+let nav_btns = document.querySelectorAll(".nav-btn")
+img_container.addEventListener("scroll", ()=>{
+    const scroll_pos = img_container.scrollLeft
+
+    nav_btns.forEach(nav_btn=>{
+        nav_btn.classList.remove("active")
+    })
+
+    if (scroll_pos<200){
+        nav_btns[0].classList.add("active")
+    }
+    else if (scroll_pos<400){
+        nav_btns[1].classList.add("active")
+    }
+    else if (scroll_pos<600){
+        nav_btns[2].classList.add("active")
+    }
+    else if (scroll_pos<800){
+        nav_btns[3].classList.add("active")
+    }
+    else if (scroll_pos<1000){
+        nav_btns[4].classList.add("active")
+    }
+    else if (scroll_pos<1191){
+        nav_btns[5].classList.add("active")
+    }
+    else if (scroll_pos>=1191){
+        nav_btns[6].classList.add("active")
+    }
 })
 
+// Scroll onclick on active image indicator buttons
+nav_btns.forEach((nav_btn, i) => {
+    nav_btn.addEventListener("click", ()=>{
+        img_container.scrollLeft = i*200
+    })
+})
