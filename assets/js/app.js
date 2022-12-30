fcf656a590200bf7f0d8d09d58e1e41aaa2fc850
AntaresMugisho <antaresmugisho@gmail.com>
2022-12-30 19:10:14 +0200

    Apply corrections and allow auto update year in the foorter

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,26 @@
 //| NAV MENU ON MOBILE             |
 //+--------------------------------+
 
+let start_year = 2022
+let years_label = document.querySelector(".years")
+let year = (new Date).getFullYear()
+
+// If user time is incorrect, then use UTC time
+if (year < start_year){
+    year = (new Date).getUTCFullYear()
+}
+
+if (year == start_year){
+    years_label.innerText = start_year
+}
+else{
+    years_label.innerText = `${start_year} - ${year}`
+}
+
+//+--------------------------------+
+//| NAV MENU ON MOBILE             |
+//+--------------------------------+
+
 // Humburger menu toggle on mobile
 const humburger = document.querySelector(".humburger")
 humburger.addEventListener("click", toggle_menu)
